c533b642ff102e4c531a66a8eda5ac6e026d5284
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-03-11 12:41:34 +0530

    Organised Code

---
+++ src/ignore.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int foo()
+{
+    return 1;
+}
+int main()
+{
+    // printf("Hello World");
+    printf("%d\n",foo());
+    int foo=10;
+    printf("%d\n",foo);    
+    return 0;
+}

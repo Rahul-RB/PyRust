24c359a9f3420d25d66dfdf9a7e6cac2932fbddd
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-03-15 20:19:55 +0530

    Added Grammar Specification

--- src/testFile.rs
+++ src/testFile.rs
@@ -5,10 +5,4 @@ fn main() {
 		this is one type of commetn
 	*/
 	panic!("asdfasdf");
-	/* ** */
-	/*//*/ 
-	/*/**/ 
-	////
-	/// This is a doc comment type 1 	
-	//! This is a doc comment type 2 
 }
